795145d7ece4faecbfca9b7703667def9bbbf67e
lucialemdar <133903013+lucialemdar@users.noreply.github.com>
2024-08-23 20:46:32 +0300

    Update ping.js

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -2,11 +2,11 @@ const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
 const prefix = process.env.PREFIX;
 exports.run = async (client, message, args) => {
   
-  const yt = new MessageButton().setLabel('Parsher Youtube').setStyle('LINK').setURL('http://youtube.com/@parsher_');
-  const dc = new MessageButton().setLabel('Parsher Discord').setStyle('LINK').setURL('https://discord.gg/bdfd');
+  const yt = new MessageButton().setLabel('Lucia Youtube').setStyle('LINK').setURL('http://youtube.com/@lucialemdar');
+  const dc = new MessageButton().setLabel('Lucia Discord').setStyle('LINK').setURL('https://discord.gg/czXgaRZNmB');
   const row = new MessageActionRow().addComponents(dc, yt)
   const embed = new MessageEmbed()
-  .setTitle(`Parsher v13 Kayıt Botu`)
+  .setTitle(`Frozan | Kayıt Botu`)
   .addField(`> Pingim`,`${client.ws.ping}ms`,false)
   .setFooter(`${message.author.tag} tarafından istendi`, message.author.avatarURL({dynamic:true}))
   .setColor('BLUE')
